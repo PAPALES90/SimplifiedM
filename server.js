71bc681157c1bc9b9601813a35db3ab5842d41ff
PAPALES90 <usluomerfaruk12@gmail.com>
2026-07-03 22:06:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,27 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// index.html'i sunmak için
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+// Kullanıcıları tutacağımız liste
+let aktifKullanicilar = {}; 
+
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
 
 io.on('connection', (socket) => {
-  console.log('Birisi bağlandı');
-  socket.on('mesaj', (veri) => {
-    io.emit('mesaj', veri);
-  });
+    // Kullanıcı bağlanınca ismini al
+    socket.on('yeni-kullanici', (isim) => {
+        aktifKullanicilar[socket.id] = isim;
+        io.emit('kullanici-listesi', Object.values(aktifKullanicilar));
+        io.emit('mesaj', `Sistem: ${isim} sohbete katıldı.`);
+    });
+
+    socket.on('mesaj', (veri) => { io.emit('mesaj', veri); });
+
+    socket.on('disconnect', () => {
+        const isim = aktifKullanicilar[socket.id];
+        delete aktifKullanicilar[socket.id];
+        io.emit('kullanici-listesi', Object.values(aktifKullanicilar));
+        if(isim) io.emit('mesaj', `Sistem: ${isim} ayrıldı.`);
+    });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Sunucu ayakta!'));
+http.listen(3000, () => console.log('Sunucu ayakta!'));
