ba41dbe7b9deb24f0082c9a95d1ea6b0a2a85ee5
PAPALES90 <usluomerfaruk12@gmail.com>
2026-07-04 10:46:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,26 +4,34 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// Kullanıcıları tutacağımız liste
-let aktifKullanicilar = {}; 
+let aktifKullanicilar = {};
+let mesajGecmisi = []; // Mesajları burada tutacağız (Kalıcı hafıza için veritabanı gerekir)
 
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
 
 io.on('connection', (socket) => {
-    // Kullanıcı bağlanınca ismini al
+    
     socket.on('yeni-kullanici', (isim) => {
         aktifKullanicilar[socket.id] = isim;
+        
+        // Yeni giren kişiye geçmiş mesajları gönder
+        socket.emit('gecmis-mesajlar', mesajGecmisi);
+        
         io.emit('kullanici-listesi', Object.values(aktifKullanicilar));
-        io.emit('mesaj', `Sistem: ${isim} sohbete katıldı.`);
+        io.emit('mesaj', {kimden: "Sistem", icerik: `${isim} sohbete katıldı.`});
     });
 
-    socket.on('mesaj', (veri) => { io.emit('mesaj', veri); });
+    socket.on('mesaj', (veri) => {
+        const yeniMesaj = { kimden: veri.isim, icerik: veri.mesaj };
+        mesajGecmisi.push(yeniMesaj); // Sepete ekle
+        io.emit('mesaj', yeniMesaj); // Herkese gönder
+    });
 
     socket.on('disconnect', () => {
         const isim = aktifKullanicilar[socket.id];
         delete aktifKullanicilar[socket.id];
         io.emit('kullanici-listesi', Object.values(aktifKullanicilar));
-        if(isim) io.emit('mesaj', `Sistem: ${isim} ayrıldı.`);
+        if(isim) io.emit('mesaj', {kimden: "Sistem", icerik: `${isim} ayrıldı.`});
     });
 });
 
